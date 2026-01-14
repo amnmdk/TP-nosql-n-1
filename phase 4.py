6a32593a2520754a038ca1e60a98758d46118139
amnmdk <objectifesgi@gmail.com>
2026-01-14 18:53:21 +0100

    phase 4 - sans fioritures

--- phase 4.py
+++ phase 4.py
@@ -6,9 +6,8 @@ for arbre in collection.find(
     {"commune": "Bois de Vincennes"}
 ).limit(10):
     print(
-        f"{arbre['nom']} ({arbre['latin']}) | "
+        f"{arbre['nom']} | "
         f"{arbre['hauteur']} m | "
-        f"{arbre['commune']}"
     )
 
 print("\n=== Arbres de plus de 20 mètres ===\n")
@@ -17,7 +16,6 @@ for arbre in collection.find(
     {"hauteur": {"$gt": 20}}
 ):
     print(
-        f"{arbre['nom']} ({arbre['latin']}) | "
+        f"{arbre['nom']} | "
         f"{arbre['hauteur']} m | "
-        f"{arbre['commune']}"
     )
