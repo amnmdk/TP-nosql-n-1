8a455bf4275548b985fbd3f23518d14485bde222
amnmdk <objectifesgi@gmail.com>
2026-01-14 18:11:17 +0100

    phase 3 - meilleur affichage

--- afficher_arbres.py
+++ afficher_arbres.py
@@ -1,4 +1,10 @@
 from config import collection
 
-for arbre in collection.find():
-    print(arbre)
+for arbre in collection.find().limit(20):
+    print(
+        f"- {arbre['nom']} ({arbre['latin']}) | "
+        f"{arbre['commune']} | "
+        f"{arbre['hauteur']} m | "
+        f"{arbre['circonference']} m | "
+        f"lat={arbre['localisation']['lat']}, lon={arbre['localisation']['lon']}"
+    )
