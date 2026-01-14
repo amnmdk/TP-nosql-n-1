51b91b2fb7aaf5a9fc607962b74d0e9814615f91
amnmdk <objectifesgi@gmail.com>
2026-01-14 18:28:42 +0100

    phase 3 - affichage corrigé par Mr Carlier

--- afficher_arbres.py
+++ afficher_arbres.py
@@ -3,8 +3,4 @@ from config import collection
 for arbre in collection.find().limit(20):
     print(
         f"- {arbre['nom']} ({arbre['latin']}) | "
-        f"{arbre['commune']} | "
-        f"{arbre['hauteur']} m | "
-        f"{arbre['circonference']} m | "
-        f"lat={arbre['localisation']['lat']}, lon={arbre['localisation']['lon']}"
     )
